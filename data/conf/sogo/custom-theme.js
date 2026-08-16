948e7af1f4ebe63b9d71679714369a6d4e4ee861
mgomon <mgomon@glassnetworks.net>
2026-08-16 17:15:15 -0400

    Add native dark theme for SOGo

--- data/conf/sogo/custom-theme.js
+++ data/conf/sogo/custom-theme.js
@@ -1,36 +1,29 @@
-/* EXAMPLE - EXAMPLE - EXAMPLE - EXAMPLE - EXAMPLE - EXAMPLE - EXAMPLE
 (function() {
   'use strict';
+
   angular.module('SOGo.Common')
-    .config(configure)
+    .config(configure);
 
   configure.$inject = ['$mdThemingProvider'];
+
   function configure($mdThemingProvider) {
-    var greyMap = $mdThemingProvider.extendPalette('grey', {
-      '200': 'F5F5F5',
-      '300': 'E5E5E5',
-      '1000': '4C566A'
-    });
-    var greenCow = $mdThemingProvider.extendPalette('green', {
-      '600': 'E5E5E5'
-    });
-    $mdThemingProvider.definePalette('frost-grey', greyMap);
-    $mdThemingProvider.definePalette('green-cow', greenCow);
     $mdThemingProvider.theme('default')
-      .primaryPalette('green-cow', {
-        'default': '400',
-        'hue-1': '400',
-        'hue-2': '600',
+      .primaryPalette('sogo-blue', {
+        'default': '900',
+        'hue-1': '700',
+        'hue-2': '800',
         'hue-3': 'A700'
       })
-      .accentPalette('green', {
-        'default': '600',
-        'hue-1': '300',
-        'hue-2': '300',
+      .accentPalette('amber', {
+        'default': 'A200',
+        'hue-1': '200',
+        'hue-2': 'A400',
         'hue-3': 'A700'
       })
-      .backgroundPalette('frost-grey');
+      .backgroundPalette('sogo-grey')
+      .dark();
+
+    // The bundled theme stylesheet is light, so generate the dark rules at startup.
     $mdThemingProvider.generateThemesOnDemand(false);
   }
 })();
- */
